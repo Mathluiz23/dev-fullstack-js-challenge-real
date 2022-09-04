89c47d2f309c4c9c298d9206b8d03dd5c8ef822a
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-09-04 15:40:24 -0300

    Implementing students database

---
+++ backend/src/database.js
@@ -0,0 +1,33 @@
+module.exports = [
+  {
+    nome: "Matheus Luiz",
+    email: "matheusluiz.dev@gmail.com",
+    cpf: "123456-789",
+    ra:"3385"
+  },
+  {
+    nome: "Luiz Carlos",
+    email: "luizcarlos@gmail.com",
+    cpf: "102167-789",
+    ra:"3312"
+  },
+  {
+    nome: "Neiva Regina",
+    email: "neiva@gmail.com",
+    cpf: "123123-789",
+    ra:"3155"
+  },
+  {
+    nome: "Jessica Aline",
+    email: "jessica@gmail.com",
+    cpf: "123036-789",
+    ra:"3265"
+  },
+  {
+    nome: "Teste acrescentando usduário",
+    email: "teste@gmail.com",
+    cpf: "1111136-789",
+    ra:"3215"
+  },
+];
+
