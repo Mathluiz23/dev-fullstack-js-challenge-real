5952fdee6346414c3ebd408d97bb8398c688f4a2
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-09-10 09:43:30 -0300

    Modifying params to string

--- backend/src/database.js
+++ backend/src/database.js
@@ -3,25 +3,25 @@ module.exports = [
     nome: "Matheus Luiz",
     email: "matheusluiz.dev@gmail.com",
     cpf: "123456789",
-    ra:"3385"
+    ra: "3385",
   },
   {
     nome: "Luiz Carlos",
     email: "luizcarlos@gmail.com",
     cpf: "102167789",
-    ra:"3312"
+    ra: "3312",
   },
   {
     nome: "Neiva Regina",
     email: "neiva@gmail.com",
     cpf: "123123789",
-    ra:"3155"
+    ra: "3155",
   },
   {
     nome: "Teste acrescentando usduário",
     email: "teste@gmail.com",
     cpf: "1111136789",
-    ra:"3215"
+    ra: "3215",
   },
 ];
 
