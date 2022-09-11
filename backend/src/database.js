7f21eeb702851cd5cdadc2eec15d769f1d528093
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-09-11 08:13:12 -0300

    Implementing css style

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,27 +1,26 @@
 module.exports = [
-  {
-    nome: "Matheus Luiz",
-    email: "matheusluiz.dev@gmail.com",
-    cpf: "123456789",
-    ra: "3385",
-  },
-  {
-    nome: "Luiz Carlos",
-    email: "luizcarlos@gmail.com",
-    cpf: "102167789",
-    ra: "3312",
-  },
-  {
-    nome: "Neiva Regina",
-    email: "neiva@gmail.com",
-    cpf: "123123789",
-    ra: "3155",
-  },
-  {
-    nome: "Teste acrescentando usduário",
-    email: "teste@gmail.com",
-    cpf: "1111136789",
-    ra: "3215",
-  },
+	{
+		nome: "Matheus Luiz",
+		email: "matheusluiz.dev@gmail.com",
+		cpf: "123456789",
+		ra: "3385",
+	},
+	{
+		nome: "Luiz Carlos",
+		email: "luizcarlos@gmail.com",
+		cpf: "102167789",
+		ra: "3312",
+	},
+	{
+		nome: "Neiva Regina",
+		email: "neiva@gmail.com",
+		cpf: "123123789",
+		ra: "3155",
+	},
+	{
+		nome: "Teste acrescentando usduário",
+		email: "teste@gmail.com",
+		cpf: "1111136789",
+		ra: "3215",
+	},
 ];
-
