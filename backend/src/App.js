2cb8cbc496c9dd4f87226dbbefb9f4ae5b066fc7
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-09-11 08:10:37 -0300

    Implementing verb post to add new student in database and adjusting other parameters

--- backend/src/App.js
+++ backend/src/App.js
@@ -1,42 +1,56 @@
-const express = require('express');
-const database = require("./database")
-const cors = require('cors');
-console.log(database)
-
+const express = require("express");
+let database = require("./database");
+const cors = require("cors");
+console.log(database);
 
 const app = express();
-
 
 app.use(cors());
 
 app.use(express.json());
 
-
-
-
 app.get("/students/list", (req, res) => {
-  setTimeout(()=> {
-    res.send(database);
-  }, 2000);
-// after 2 seconds get a database, simulation bd request 
+	setTimeout(() => {
+		res.send(database);
+	}, 1500);
+	// after 2 seconds get a database, simulation bd request
 });
 
 app.get("/students/find/:ra", (req, res) => {
-  const studentsFound = database.find((student) => {
-    return student.ra === req.params.ra;
-  });
+	const studentsFound = database.find((student) => {
+		return student.ra === req.params.ra;
+	});
 
-  setTimeout(() => {
-    res.send(studentsFound);
-  }, 2000)
+	setTimeout(() => {
+		res.send(studentsFound);
+	}, 1500);
 });
 
 app.delete("/students/delete/:ra", (req, res) => {
-  res.send({result: `O estudante #${req.params.ra} foi excluído com sucesso`});
+	database = database.filter((student) => {
+		return student.ra !== req.params.ra;
+	});
+	res.send({
+		result: true,
+		message: `O estudante ${req.params.ra} foi excluído com sucesso`,
+	});
 });
 
+app.post("/students/newstudent", (req, res) => {
+	console.log(req.body);
+	database.push({
+		nome: req.body.name,
+		ra: req.body.ra,
+		email: req.body.email,
+		cpf: req.body.cpf,
+	});
+	res.send({
+		result: true,
+		message: `Aluno ${req.body.name} foi cadastrado com sucesso`,
+	});
+});
 
 app.listen(3000);
-console.log("Server is running...")
+console.log("Server is running...");
 
 module.exports = app;
