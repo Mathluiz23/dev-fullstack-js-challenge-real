863dd9a62a68f14d1da64441a12368ce5ff2150b
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-10-05 06:02:50 -0300

    :sparkles: feat Installing knex and configuring file knexfile to communicate with bd

---
+++ backend/knexfile.js
@@ -0,0 +1,14 @@
+module.exports = {
+	test: {
+		client: "mysql",
+		connection: {
+			host: "remotemysql.com",
+			user: "982zYZykWO",
+			password: "20Blrvffro",
+			database: "982zYZykWO",
+		},
+		migratons: {
+			directory: "migrations",
+		},
+	},
+};
