5d1d35454475c96f9005bdc07ff885323a33c0fc
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-10-09 16:57:20 -0300

    :recycle: feat implementing default route and migrating verbs from app to routes folder

--- backend/src/App.js
+++ backend/src/App.js
@@ -3,23 +3,13 @@ const cors = require("cors");
 const knex = require("knex");
 const knexConfigFile = require("../knexfile");
 const app = express();
-const studentsController = require("./controllers/studentController");
+const studentsRoutes = require("./routes/studentsRoutes");
 
 app.use(cors());
 app.database = knex(knexConfigFile.test);
 app.use(express.json());
 
-const studentsControllerInstance = new studentsController(app);
-
-app.get("/students/list/:searchQuery?", studentsControllerInstance.listAction);
-
-app.get("/students/find/:ra", studentsControllerInstance.findAction);
-
-app.put("/students/editstudent/:ra", studentsControllerInstance.editAction);
-
-app.delete("/students/delete/:ra", studentsControllerInstance.deleteAction);
-
-app.post("/students/newstudent", studentsControllerInstance.createAction);
+app.use("/students", studentsRoutes(app));
 
 app.listen(3000);
 console.log("Server is running...");
