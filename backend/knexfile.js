d8ac8dc6579ef572722a9cb8cb758088444ffb7b
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-10-10 07:01:03 -0300

    :recycle: feat setting file to hide password

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
+
 module.exports = {
 	test: {
 		client: "mysql",
 		connection: {
 			host: "remotemysql.com",
 			user: "982zYZykWO",
-			password: "20Blrvffro",
+			password: process.env.DB_PASS,
 			database: "982zYZykWO",
 		},
 		migratons: {
